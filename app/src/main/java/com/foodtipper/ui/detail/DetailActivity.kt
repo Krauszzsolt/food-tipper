d0c5dcfd6e57f8e8dded4f7ac9f8af2318c26a6d
Krausz Zsolt <krausz.zsolt97@gmail.com>
2021-04-08 10:25:54 +0200

    Empty activity and Foodlist Fragment

---
+++ app/src/main/java/com/foodtipper/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.foodtipper.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foodtipper.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detail)
+    }
+}
---
+++ app/src/main/java/com/foodtipper/ui/edit/EditActivity.kt
@@ -0,0 +1,12 @@
+package com.foodtipper.ui.edit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foodtipper.R
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit)
+    }
+}
