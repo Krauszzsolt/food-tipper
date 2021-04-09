673d3599d7f81d24ff9f9f42d30bbb4e0543bf77
Krausz Zsolt <krausz.zsolt97@gmail.com>
2021-04-09 13:24:42 +0200

    Skeleton Networkconfig

--- app/src/main/java/com/foodtipper/ui/injector.kt
+++ app/src/main/java/com/foodtipper/ui/injector.kt
@@ -2,6 +2,7 @@ package com.foodtipper.ui
 
 
 import android.app.Activity
+import androidx.fragment.app.Fragment
 import com.foodtipper.FoodTipperApplication
 import com.foodtipper.FoodTipperApplicationComponent
 
@@ -11,3 +12,7 @@ val Activity.injector: FoodTipperApplicationComponent
         return (this.applicationContext as FoodTipperApplication).injector
     }
 
+val Fragment.injector: FoodTipperApplicationComponent
+    get() {
+        return (this.context!!.applicationContext as FoodTipperApplication).injector
+    }
