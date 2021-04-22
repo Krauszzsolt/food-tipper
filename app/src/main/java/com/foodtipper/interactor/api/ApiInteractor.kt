de63aad042d15ff9f48d670368cfa95bb531d35b
Krausz Zsolt <krausz.zsolt97@gmail.com>
2021-04-22 10:37:02 +0200

    Interactor frame

--- app/src/main/java/com/foodtipper/interactor/api/ApiInteractor.kt
+++ app/src/main/java/com/foodtipper/interactor/api/ApiInteractor.kt
@@ -1,7 +1,11 @@
 package com.foodtipper.interactor.api
 
+import com.foodtipper.interactor.api.event.DeleteFoodApiEvent
 import com.foodtipper.interactor.api.event.GetFoodDetailApiEvent
+import com.foodtipper.interactor.api.event.GetFoodsApiEvent
+import com.foodtipper.interactor.api.event.PostFoodApiEvent
 import com.foodtipper.model.FoodDetails
+import com.foodtipper.model.FoodItem
 import org.greenrobot.eventbus.EventBus
 
 class ApiInteractor {
@@ -21,14 +25,37 @@ class ApiInteractor {
     }
 
     fun getFoods() {
+        val event = GetFoodsApiEvent()
 
+        try {
+            event.foods =
+                listOf(FoodItem("Teszt", "Teszt", "Teszt"), FoodItem("Teszt", "Teszt", "Teszt"))
+            EventBus.getDefault().post(event)
+
+        } catch (e: Exception) {
+            event.exception = e
+            EventBus.getDefault().post(event)
+        }
     }
 
     fun editFood() {
-
+        val event = PostFoodApiEvent()
+        try {
+            EventBus.getDefault().post(event)
+        } catch (e: Exception) {
+            event.exception = e
+            EventBus.getDefault().post(event)
+        }
     }
 
     fun deleteFood() {
+        val event = DeleteFoodApiEvent()
+        try {
 
+            EventBus.getDefault().post(event)
+        } catch (e: Exception) {
+            event.exception = e
+            EventBus.getDefault().post(event)
+        }
     }
 }
--- app/src/main/java/com/foodtipper/interactor/persistence/PersistenceInteractor.kt
+++ app/src/main/java/com/foodtipper/interactor/persistence/PersistenceInteractor.kt
@@ -1,7 +1,11 @@
 package com.foodtipper.interactor.persistence
 
+import com.foodtipper.interactor.persistence.event.DeleteFoodPersistenceEvent
 import com.foodtipper.interactor.persistence.event.GetFoodDetailPersistenceEvent
+import com.foodtipper.interactor.persistence.event.GetFoodsPersistenceEvent
+import com.foodtipper.interactor.persistence.event.PostFoodPersistenceEvent
 import com.foodtipper.model.FoodDetails
+import com.foodtipper.model.FoodItem
 import org.greenrobot.eventbus.EventBus
 
 class PersistenceInteractor {
@@ -20,14 +24,37 @@ class PersistenceInteractor {
     }
 
     fun getFoods() {
+        val event = GetFoodsPersistenceEvent()
 
+        try {
+            event.foods =
+                listOf(FoodItem("Teszt", "Teszt", "Teszt"), FoodItem("Teszt", "Teszt", "Teszt"))
+            EventBus.getDefault().post(event)
+
+        } catch (e: Exception) {
+            event.exception = e
+            EventBus.getDefault().post(event)
+        }
     }
 
     fun editFood() {
-
+        val event = PostFoodPersistenceEvent()
+        try {
+            EventBus.getDefault().post(event)
+        } catch (e: Exception) {
+            event.exception = e
+            EventBus.getDefault().post(event)
+        }
     }
 
     fun deleteFood() {
+        val event = DeleteFoodPersistenceEvent()
+        try {
 
+            EventBus.getDefault().post(event)
+        } catch (e: Exception) {
+            event.exception = e
+            EventBus.getDefault().post(event)
+        }
     }
 }
